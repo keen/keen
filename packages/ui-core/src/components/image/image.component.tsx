049216dc16025a2b63ef8b0a4eb9b1614e36e3e1
Artur Pryka <artur.pryka@keen.io>
2020-06-08 14:05:27 +0200

    feat: 🎸 image component enhancements

--- packages/ui-core/src/components/image/image.component.tsx
+++ packages/ui-core/src/components/image/image.component.tsx
@@ -1,19 +1,40 @@
-import React, { FC, useContext } from 'react';
+import React, { FC, useContext, useState } from 'react';
+import { Loader } from '@keen.io/ui-core';
 
 import { CDNContext } from '../../context';
 
 type Props = {
   /** Resource name */
   name: string;
+  /**  Source path */
+  src?: string;
   /** Description */
   alt?: string;
+  /** Resource initial height */
+  initialHeight?: number;
+  /** Show loader when resource is loading */
+  useLoader?: boolean;
 };
 
-export const Image: FC<Props> = ({ name, alt }) => {
+export const Image: FC<Props> = ({
+  name,
+  src,
+  alt,
+  initialHeight = 22,
+  useLoader,
+}) => {
   const { url } = useContext(CDNContext);
-  console.log('dd', `${url}/${name}`);
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const imageUrl = src ? src : `${url}/${name}`;
 
-  return <img src={`${url}/${name}`} alt={alt} />;
+  return (
+    <>
+      {useLoader && !imageLoaded && (
+        <Loader width={initialHeight} height={initialHeight} />
+      )}
+      <img src={imageUrl} alt={alt} onLoad={() => setImageLoaded(true)} />
+    </>
+  );
 };
 
 export default Image;
