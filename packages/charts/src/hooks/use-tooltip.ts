1dcde88696d6870716d04f770e601ae543f5a08d
Maciej Rybaniec <maciej.rybaniec@gmail.com>
2020-07-13 10:56:19 +0200

    test: 💍 useTooltip hook unit tests

--- packages/charts/src/hooks/use-tooltip.ts
+++ packages/charts/src/hooks/use-tooltip.ts
@@ -1,6 +1,6 @@
 import React, { MutableRefObject, useRef, useState, useCallback } from 'react';
 
-import { TooltipState, DataSelector } from './types';
+import { TooltipState, DataSelector } from '../types';
 
 export const useTooltip = (
   container: MutableRefObject<any>,
