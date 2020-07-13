4705f35ca974759fe0f429357884b5e86182b472
Maciej Rybaniec <maciej.rybaniec@gmail.com>
2020-07-13 11:33:41 +0200

    test: 💍 unit tests for widget hooks

--- packages/widgets/src/hooks/use-slider.hook.ts
+++ packages/widgets/src/hooks/use-slider.hook.ts
@@ -1,20 +1,28 @@
+import { useMemo } from 'react';
 import { max as getMax, min as getMin } from 'd3-array';
 
 export const useSlider = (
   data: Record<string, any>[],
   keys: string[]
 ): { min: number; max: number } => {
-  const dataArray = [] as number[];
-  data.forEach(item => {
-    for (const property in item) {
-      if (typeof item[property] === 'number' && keys.includes(property)) {
-        dataArray.push(item[property]);
+  const { min, max } = useMemo(() => {
+    const dataArray = [] as number[];
+    data.forEach(item => {
+      for (const property in item) {
+        if (typeof item[property] === 'number' && keys.includes(property)) {
+          dataArray.push(item[property]);
+        }
       }
-    }
-  });
+    });
+
+    return {
+      min: getMin(dataArray),
+      max: getMax(dataArray),
+    };
+  }, [keys, data]);
 
   return {
-    min: getMin(dataArray),
-    max: getMax(dataArray),
+    min,
+    max,
   };
 };
