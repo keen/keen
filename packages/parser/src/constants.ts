41e6979d84288ab78fe559e9fafc5ff3b21f8597
Maciej Rybaniec <maciej.rybaniec@gmail.com>
2020-11-05 14:08:49 +0100

    test: 💍 refactor unit tests

--- packages/parser/src/constants.ts
+++ packages/parser/src/constants.ts
@@ -1,8 +1,4 @@
-import { Analysis } from './types';
-
 export const KEEN_VALUE = 'keen.value';
 export const KEEN_KEY = 'keen.key';
 
 export const DEFAULT_NAME = 'Result';
-
-export const USE_SEPARATOR_FOR_GROUPS: Analysis[] = ['select_unique'];
