c520a5d70dcb9597731c7d4fb0efe81c4f11dc2a
Maciej Rybaniec <maciej.rybaniec@gmail.com>
2020-12-13 11:00:10 +0100

    test: 💍 time scale range tests

--- packages/charts-utils/src/scale/get-scale-values/get-scale-values.test.ts
+++ packages/charts-utils/src/scale/get-scale-values/get-scale-values.test.ts
@@ -1,16 +1,21 @@
 import { scaleBand, scaleLinear, scaleTime } from 'd3-scale';
+import {
+  register as registerTimezone,
+  unregister as unregisterTimezone,
+} from 'timezone-mock';
 
 import getScaleValues from './get-scale-values';
-import getTimeScaleValues from '../get-time-scale-values';
-
-jest.mock('../get-time-scale-values');
 
 import { ScaleSettings } from '../../types';
 
 const domain = ['Sales', 'Marketing', 'E-commerce'];
 
-beforeEach(() => {
-  getTimeScaleValues.mockReset();
+beforeAll(() => {
+  registerTimezone('UTC');
+});
+
+afterAll(() => {
+  unregisterTimezone();
 });
 
 test('returns domain for scale band', () => {
@@ -19,7 +24,7 @@ test('returns domain for scale band', () => {
   expect(getScaleValues(scale)).toEqual(domain);
 });
 
-test('get values from "linear" scale', () => {
+test('returns values from "linear" scale', () => {
   const scale = scaleLinear()
     .range([0, 10])
     .domain([0, 10]);
@@ -27,9 +32,29 @@ test('get values from "linear" scale', () => {
   expect(getScaleValues(scale)).toMatchSnapshot();
 });
 
-test('calls "getTimeScaleValues()" to generate time scale values', () => {
-  const firstDate = new Date('2020-01-06T15:00:00.000Z');
-  const lastDate = new Date('2020-08-06T15:30:00.000Z');
+test('returns time scale values in yearly range', () => {
+  const firstDate = new Date('2017-01-01T00:00:00.000Z');
+  const lastDate = new Date('2020-01-01T00:00:00.000Z');
+
+  const scale = scaleTime()
+    .range([0, 10])
+    .domain([firstDate, lastDate]);
+
+  const scaleSettings: ScaleSettings = { type: 'time', precision: 'year' };
+
+  expect(getScaleValues(scale, scaleSettings)).toMatchInlineSnapshot(`
+    Array [
+      "2017-01-01T00:00:00.000Z",
+      "2018-01-01T00:00:00.000Z",
+      "2019-01-01T00:00:00.000Z",
+      "2020-01-01T00:00:00.000Z",
+    ]
+  `);
+});
+
+test('returns time scale values in monthly range', () => {
+  const firstDate = new Date('2020-01-01T00:00:00.000Z');
+  const lastDate = new Date('2020-03-01T00:00:00.000Z');
 
   const scale = scaleTime()
     .range([0, 10])
@@ -37,7 +62,122 @@ test('calls "getTimeScaleValues()" to generate time scale values', () => {
 
   const scaleSettings: ScaleSettings = { type: 'time', precision: 'month' };
 
-  getScaleValues(scale, scaleSettings);
+  expect(getScaleValues(scale, scaleSettings)).toMatchInlineSnapshot(`
+    Array [
+      "2020-01-01T00:00:00.000Z",
+      "2020-02-01T00:00:00.000Z",
+      "2020-03-01T00:00:00.000Z",
+    ]
+  `);
+});
 
-  expect(getTimeScaleValues).toHaveBeenCalledWith(scale, scaleSettings);
+test('returns time scale values in weekly range', () => {
+  const firstDate = new Date('2020-01-15T00:00:00.000Z');
+  const lastDate = new Date('2020-02-15T00:00:00.000Z');
+
+  const scale = scaleTime()
+    .range([0, 10])
+    .domain([firstDate, lastDate]);
+
+  const scaleSettings: ScaleSettings = { type: 'time', precision: 'week' };
+
+  expect(getScaleValues(scale, scaleSettings)).toMatchInlineSnapshot(`
+    Array [
+      "2020-01-19T00:00:00.000Z",
+      "2020-01-26T00:00:00.000Z",
+      "2020-02-02T00:00:00.000Z",
+      "2020-02-09T00:00:00.000Z",
+      "2020-02-15T00:00:00.000Z",
+    ]
+  `);
+});
+
+test('returns time scale values in daily range', () => {
+  const firstDate = new Date('2020-01-01T00:00:00.000Z');
+  const lastDate = new Date('2020-01-05T00:00:00.000Z');
+
+  const scale = scaleTime()
+    .range([0, 10])
+    .domain([firstDate, lastDate]);
+
+  const scaleSettings: ScaleSettings = { type: 'time', precision: 'day' };
+
+  expect(getScaleValues(scale, scaleSettings)).toMatchInlineSnapshot(`
+    Array [
+      "2020-01-01T00:00:00.000Z",
+      "2020-01-02T00:00:00.000Z",
+      "2020-01-03T00:00:00.000Z",
+      "2020-01-04T00:00:00.000Z",
+      "2020-01-05T00:00:00.000Z",
+    ]
+  `);
+});
+
+test('returns time scale values in hours range', () => {
+  const firstDate = new Date('2020-01-15T12:00:00.000Z');
+  const lastDate = new Date('2020-01-15T16:00:00.000Z');
+
+  const scale = scaleTime()
+    .range([0, 10])
+    .domain([firstDate, lastDate]);
+
+  const scaleSettings: ScaleSettings = { type: 'time', precision: 'hour' };
+
+  expect(getScaleValues(scale, scaleSettings)).toMatchInlineSnapshot(`
+    Array [
+      "2020-01-15T12:00:00.000Z",
+      "2020-01-15T13:00:00.000Z",
+      "2020-01-15T14:00:00.000Z",
+      "2020-01-15T15:00:00.000Z",
+      "2020-01-15T16:00:00.000Z",
+    ]
+  `);
+});
+
+test('returns time scale values in minute range', () => {
+  const firstDate = new Date('2020-01-15T12:00:00.000Z');
+  const lastDate = new Date('2020-01-15T12:05:00.000Z');
+
+  const scale = scaleTime()
+    .range([0, 10])
+    .domain([firstDate, lastDate]);
+
+  const scaleSettings: ScaleSettings = { type: 'time', precision: 'minute' };
+
+  expect(getScaleValues(scale, scaleSettings)).toMatchInlineSnapshot(`
+    Array [
+      "2020-01-15T12:00:00.000Z",
+      "2020-01-15T12:01:00.000Z",
+      "2020-01-15T12:02:00.000Z",
+      "2020-01-15T12:03:00.000Z",
+      "2020-01-15T12:04:00.000Z",
+      "2020-01-15T12:05:00.000Z",
+    ]
+  `);
+});
+
+test('returns time scale values in custom range', () => {
+  const firstDate = new Date('2020-01-02T00:00:00.000Z');
+  const lastDate = new Date('2020-01-15T00:00:00.000Z');
+
+  const scale = scaleTime()
+    .range([0, 10])
+    .domain([firstDate, lastDate]);
+
+  const scaleSettings: ScaleSettings = {
+    type: 'time',
+    precision: 'day',
+    stepRange: 3,
+  };
+
+  expect(getScaleValues(scale, scaleSettings)).toMatchInlineSnapshot(`
+    Array [
+      "2020-01-02T00:00:00.000Z",
+      "2020-01-05T00:00:00.000Z",
+      "2020-01-08T00:00:00.000Z",
+      "2020-01-11T00:00:00.000Z",
+      "2020-01-14T00:00:00.000Z",
+      "2020-01-15T00:00:00.000Z",
+    ]
+  `);
 });
