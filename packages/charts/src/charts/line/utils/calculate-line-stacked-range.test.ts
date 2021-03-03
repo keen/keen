e017f1c64eed2ed2067c9dcc5c3c546250a9836f
dariuszlacheta <dariusz.lacheta@keen.io>
2021-03-03 12:10:00 +0100

    fix: 🐛 Fix for yScale for stacked area/line

---
+++ packages/charts/src/charts/line/utils/calculate-line-stacked-range.test.ts
@@ -0,0 +1,54 @@
+import calculateLineStackedRange from './calculate-line-stacked-range';
+
+test('calculates "minimum" and "maximum" values', () => {
+  const data = [
+    { amount: 10, profit: 20 },
+    { amount: -30, profit: 10 },
+    { amount: 20, profit: 100 },
+    { amount: -20, profit: 0 },
+  ];
+
+  const result = calculateLineStackedRange(data, 'auto', 'auto', [
+    'amount',
+    'profit',
+  ]);
+
+  expect(result).toMatchInlineSnapshot(`
+    Object {
+      "maximum": 120,
+      "minimum": -30,
+    }
+  `);
+});
+
+test('setup "minimum" based on provided value', () => {
+  const data = [
+    { amount: 10 },
+    { amount: 30 },
+    { amount: 20 },
+    { amount: -15 },
+  ];
+
+  const result = calculateLineStackedRange(data, -40, 'auto', ['amount']);
+
+  expect(result).toEqual({
+    minimum: -40,
+    maximum: 30,
+  });
+});
+
+test('setup "maximum" based on provided value', () => {
+  const data = [
+    { profit: 10 },
+    { profit: 300 },
+    { profit: 20 },
+    { profit: 10 },
+  ];
+
+  const result = calculateLineStackedRange(data, 'auto', 100, ['amount']);
+
+  expect(result).toEqual({
+    minimum: 0,
+    maximum: 100,
+  });
+});
