c7f1a9dc8626dee6c57ff692f5b6f882257afaa2
dariuszlacheta <dariusz.lacheta@keen.io>
2021-05-04 17:45:31 +0200

    feat: 🎸 Update translations cli

--- packages/translations-cli/src/index.ts
+++ packages/translations-cli/src/index.ts
@@ -14,7 +14,7 @@ program
   .arguments('<SOURCE_DIR> <TARGET_FILE>')
   .description('', {
     SOURCE_DIR: 'dir with translation file',
-    TARGET_FILE: 'translation file to merge with',
+    TARGET_FILE: 'translation file to merge with(url or local file)',
   })
   .action((SOURCE_DIR: string, TARGET_FILE: string) => {
     try {
@@ -22,23 +22,35 @@ program
         fs.readFileSync(`${SOURCE_DIR}/translation.json`)
       );
       let targetData = '';
-      https.get(TARGET_FILE, (resp: any) => {
-        resp.on('data', (chunk: any) => {
-          targetData += chunk;
-        });
+      if (TARGET_FILE.includes('http')) {
+        https.get(TARGET_FILE, (resp: any) => {
+          resp.on('data', (chunk: any) => {
+            targetData += chunk;
+          });
 
-        resp.on('end', () => {
-          const parsedTargetData = JSON.parse(targetData);
-          const mergedData = merge(sourceData, parsedTargetData);
-          fs.writeFile(
-            `${SOURCE_DIR}/output.json`,
-            JSON.stringify(mergedData, null, 2),
-            function () {
-              console.log(addedDiff(sourceData, mergedData));
-            }
-          );
+          resp.on('end', () => {
+            const parsedTargetData = JSON.parse(targetData);
+            const mergedData = merge(sourceData, parsedTargetData);
+            fs.writeFile(
+              `${SOURCE_DIR}/output.json`,
+              JSON.stringify(mergedData, null, 2),
+              function () {
+                console.log(addedDiff(sourceData, mergedData));
+              }
+            );
+          });
         });
-      });
+      } else {
+        targetData = JSON.parse(fs.readFileSync(`${TARGET_FILE}`));
+        const mergedData = merge(sourceData, targetData);
+        fs.writeFile(
+          `${SOURCE_DIR}/output.json`,
+          JSON.stringify(mergedData, null, 2),
+          function () {
+            console.log(addedDiff(sourceData, mergedData));
+          }
+        );
+      }
     } catch (err) {
       console.error(err);
     }
