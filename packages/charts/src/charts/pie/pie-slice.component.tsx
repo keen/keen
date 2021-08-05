80ba029c6bb6f334c3deb305c04bb16de8fcc957
dariuszlacheta <dariusz.lacheta@keen.io>
2021-08-05 09:39:15 +0200

    fix: 🐛 Build fix

--- packages/charts/src/charts/pie/pie-slice.component.tsx
+++ packages/charts/src/charts/pie/pie-slice.component.tsx
@@ -32,7 +32,7 @@ type Props = {
   autocolor: boolean;
   labelPosition: [number, number];
   activePosition: [number, number];
-  label: string | number;
+  label: string | number | Date | boolean;
   background: string;
   id: string;
   activeKey?: string;
