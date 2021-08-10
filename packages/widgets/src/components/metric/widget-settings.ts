a6c21bf8a76b5c907a9edba345e7ec843ad339f0
Maciej Rybaniec <maciej.rybaniec@gmail.com>
2021-08-10 12:10:14 +0200

    fix: 🐛 default metric title

--- packages/widgets/src/components/metric/widget-settings.ts
+++ packages/widgets/src/components/metric/widget-settings.ts
@@ -1,5 +1,7 @@
 import { colors } from '@keen.io/colors';
 
+import { widgetSettings } from '../../widget-settings';
+
 export const metricWidgetSettings = {
   card: {
     backgroundColor: colors.white['500'],
@@ -7,4 +9,6 @@ export const metricWidgetSettings = {
     border: 'none',
     hasShadow: true,
   },
+  title: widgetSettings.title,
+  subtitle: widgetSettings.subtitle,
 };
