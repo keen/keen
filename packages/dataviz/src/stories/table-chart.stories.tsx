86038ac8c0e330a10933702cd5a649cadddbdbaf
Maciej Rybaniec <maciej.rybaniec@gmail.com>
2021-09-01 12:36:02 +0200

    feat: 🎸 table events

--- packages/dataviz/src/stories/table-chart.stories.tsx
+++ packages/dataviz/src/stories/table-chart.stories.tsx
@@ -48,3 +48,40 @@ export const simpleResults = () => {
 
   return <div style={{ width: '700px', height: '500px' }} ref={container} />;
 };
+
+export const simpleResultsEditMode = () => {
+  const container = React.useRef(null);
+
+  React.useEffect(() => {
+    const client = new KeenAnalysis(analysisConfig);
+    const dataviz = new KeenDataViz({
+      type: 'table',
+      inEditMode: true,
+      container: container.current,
+      widget: {
+        title: {
+          content: 'Book purchases',
+        },
+        subtitle: {
+          content: 'hourly',
+        },
+      },
+      settings: {
+        margins: { top: 30, left: 45, right: 30, bottom: 60 },
+      },
+    });
+
+    client
+      .query({
+        analysis_type: 'extraction',
+        event_collection: 'book_purchase',
+        timeframe: {
+          start: '2020-02-01T00:00:00.000-00:00',
+          end: '2020-02-01T16:00:00.000-00:00',
+        },
+      })
+      .then((res: any) => dataviz.render(res));
+  }, []);
+
+  return <div style={{ width: '700px', height: '500px' }} ref={container} />;
+};
