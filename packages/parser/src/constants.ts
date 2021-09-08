89c14ee78b53c10f58cec5552c81c0f71d4625f3
Michał Czapkowicz <michal.czapkowicz@keen.io>
2021-09-08 11:28:52 +0200

    fix: 🐛 Table formatting fixes

--- packages/parser/src/constants.ts
+++ packages/parser/src/constants.ts
@@ -1,7 +1,7 @@
 export const KEEN_VALUE = 'keen.value';
 export const KEEN_KEY = 'keen.key';
 export const KEEN_ELEMENTS = 'keen.elements';
-export const KEEN_TABLE_INTERVAL = 'Interval';
+export const KEEN_TABLE_INTERVAL = 'interval';
 
 export const DEFAULT_NAME = 'Result';
 
