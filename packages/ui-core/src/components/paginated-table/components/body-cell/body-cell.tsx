8a7384970cb676c63a222cb81cdf2cc4df2fbe60
Michał Czapkowicz <michal.czapkowicz@keen.io>
2021-10-18 16:15:35 +0200

    fix: 🐛 CR fixes

--- packages/ui-core/src/components/paginated-table/components/body-cell/body-cell.tsx
+++ packages/ui-core/src/components/paginated-table/components/body-cell/body-cell.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useMemo } from 'react';
 import { Cell as CellType } from 'react-table';
 
 import { Text } from '../../../../typography';
 import { Typography } from '../../../../types';
-import { CellTextAlignment, CellValue, FormattedValue } from '../../types';
+import { CellValue } from '../../types';
 import { StyledCell, Container } from './body-cell.styles';
 
 type Props = {
@@ -18,26 +18,14 @@ type Props = {
   ) => void;
 };
 
-const isFormattedValue = (el: any): el is FormattedValue =>
-  typeof el === 'object' && el !== null && 'value' in el;
-
 export const BodyCell = ({ cell, typography, onCellClick, idx }: Props) => {
-  const [textAlignment, setTextAlignment] = useState<CellTextAlignment>('left');
-  const [value, setValue] = useState<CellValue>('');
+  const { value, formatterType } = cell.value;
 
-  useEffect(() => {
-    if (isFormattedValue(cell.value)) {
-      const { value, formatterType } = cell.value;
-      const alignment = formatterType === 'number' ? 'right' : 'left';
-      setTextAlignment(alignment);
-      setValue(value);
-    } else {
-      setValue(cell.value);
-    }
-    if (typeof cell.value === 'number') {
-      setTextAlignment('right');
-    }
-  }, [cell]);
+  const textAlignment = useMemo(() => {
+    return formatterType === 'number' || typeof value === 'number'
+      ? 'right'
+      : 'left';
+  }, [value, formatterType]);
 
   return (
     <StyledCell
