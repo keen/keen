93b06be66e5cf2f114b3ede8874ddcc37ac3075d
Artur Pryka <artur.pryka@keen.io>
2021-10-22 15:50:57 +0200

    test: 💍 add mock to observer

--- packages/widgets/src/components/table/table-chart.widget.test.tsx
+++ packages/widgets/src/components/table/table-chart.widget.test.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
+import { mockAllIsIntersecting } from 'react-intersection-observer/test-utils';
 import { ChartEvents } from '@keen.io/charts';
 import 'jest-styled-components';
 
@@ -30,6 +31,8 @@ const render = (overProps: any = {}) => {
     props,
   };
 };
+
+mockAllIsIntersecting(true);
 
 beforeEach(() => jest.clearAllMocks());
 
