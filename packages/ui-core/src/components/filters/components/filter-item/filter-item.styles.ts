61936ff277933d21d4a7b400005e34939879f8b1
Michał Czapkowicz <michal.czapkowicz@keen.io>
2021-10-28 09:16:10 +0200

    fix: 🐛 Obsolete overflow removed

--- packages/ui-core/src/components/filters/components/filter-item/filter-item.styles.ts
+++ packages/ui-core/src/components/filters/components/filter-item/filter-item.styles.ts
@@ -9,7 +9,6 @@ export const Container = styled.label`
 export const Label = styled.span`
   margin-left: 4px;
   width: 100%;
-  overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
 `;
