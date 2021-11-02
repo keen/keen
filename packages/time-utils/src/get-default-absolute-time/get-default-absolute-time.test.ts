f8b923c8c90f6f6a61fbb13a97ca38028583bdde
Maciej Rybaniec <maciej.rybaniec@gmail.com>
2021-11-02 18:06:57 +0100

    test: 💍 dayjs utcOffset mock

--- packages/time-utils/src/get-default-absolute-time/get-default-absolute-time.test.ts
+++ packages/time-utils/src/get-default-absolute-time/get-default-absolute-time.test.ts
@@ -1,4 +1,38 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
 import { advanceTo, clear as clearDate } from 'jest-date-mock';
+
+const utcPlugin = jest.requireActual('dayjs/plugin/utc');
+
+/**
+ * This is naive implementation of dayjs.utcOffset method.
+ * It was created to make test deterministic and timezone DST values do not affect results.
+ */
+jest.mock(
+  'dayjs/plugin/utc',
+  () => (
+    option: string,
+    Dayjs: Record<string, any>,
+    dayjs: Record<string, any>
+  ) => {
+    const proto = Dayjs.prototype;
+    const plugin = utcPlugin(option, Dayjs, dayjs);
+    const hourOffset = 60;
+
+    proto.utcOffset = function (input: number, keepLocalTime?: boolean) {
+      const instace = this;
+      if (!input) return hourOffset;
+      if (input && keepLocalTime) {
+        instace.$offset = hourOffset;
+        instace.$u = input === 0;
+        return instace;
+      }
+
+      return instace;
+    };
+
+    return plugin;
+  }
+);
 
 import getDefaultAbsoluteTime from './get-default-absolute-time';
 
@@ -11,20 +45,11 @@ afterAll(() => {
   clearDate();
 });
 
-test('get default timeframe for "UTC" timezone', () => {
+test('get default timeframe for "Europe/Warsaw" timezone', () => {
   expect(getDefaultAbsoluteTime('Europe/Warsaw')).toMatchInlineSnapshot(`
     Object {
-      "end": "2021-03-31T00:00:00+02:00",
-      "start": "2021-03-30T00:00:00+02:00",
-    }
-  `);
-});
-
-test('get default timeframe for "America/New_York" timezone', () => {
-  expect(getDefaultAbsoluteTime('America/New_York')).toMatchInlineSnapshot(`
-    Object {
-      "end": "2021-03-31T00:00:00-04:00",
-      "start": "2021-03-30T00:00:00-04:00",
+      "end": "2021-03-31T00:00:00+01:00",
+      "start": "2021-03-30T00:00:00+01:00",
     }
   `);
 });
