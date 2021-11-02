f775cfd3cbc33dcff0bbc81d83bd43f016446f83
Maciej Rybaniec <maciej.rybaniec@gmail.com>
2021-11-02 08:09:30 +0100

    feat: 🎸 column click logic

--- packages/charts/src/charts/table/components/select-column-header/select-column-header.component.tsx
+++ packages/charts/src/charts/table/components/select-column-header/select-column-header.component.tsx
@@ -4,14 +4,17 @@ import { UseRowSelectInstanceProps } from 'react-table';
 import { HeaderCellContent } from '../header-cell-content';
 import { SelectRow } from '../select-row';
 
-type Props = UseRowSelectInstanceProps<Record<string, any>>;
+type Props = UseRowSelectInstanceProps<Record<string, any>> & {
+  editMode: boolean;
+};
 
 export const SelectColumnHeader = ({
   getToggleAllRowsSelectedProps,
   toggleAllRowsSelected,
+  editMode,
 }: Props) => {
   return (
-    <HeaderCellContent onClick={() => toggleAllRowsSelected()}>
+    <HeaderCellContent onClick={() => !editMode && toggleAllRowsSelected()}>
       <SelectRow
         id="select-all-rows"
         checkboxVariant="highlight"
